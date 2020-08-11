bbbccfba37ffbec3aac67e1ca40bc19b46a47bbe
Reuben Thomas-Davis <reubenestd@gmail.com>
2020-08-11 13:58:04 +0100

    Updates

---
+++ precache-manifest.ec278e9178930144f9eb6d7fb3d30849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543b6e3db82ee8ba05f1546d56d11163",
+    "url": "./index.html"
+  },
+  {
+    "revision": "470bf44b084ef858348d",
+    "url": "./static/css/main.4e09ce3e.chunk.css"
+  },
+  {
+    "revision": "ea6aab0f3c9b8c3f2bfb",
+    "url": "./static/js/2.49f45328.chunk.js"
+  },
+  {
+    "revision": "7e5f66d80416e3c5abe118df3bd5d80f",
+    "url": "./static/js/2.49f45328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470bf44b084ef858348d",
+    "url": "./static/js/main.df515a66.chunk.js"
+  },
+  {
+    "revision": "3207052abea0eba88086",
+    "url": "./static/js/runtime-main.a9d951d7.js"
+  }
+]);
